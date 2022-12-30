02801e3e5c7c8f5f09825ca5abe564887e8f12db
Rafael Jáber <rafael.jaber@hotmail.com>
2022-12-29 22:12:06 -0300

    Criada a classe LeilaoDao.cs para transferir todo o tratamento de dados do controller para ela.

---
+++ src/Alura.LeilaoOnline.WebApp/Dados/LeilaoDao.cs
@@ -0,0 +1,64 @@
+﻿using Alura.LeilaoOnline.WebApp.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Alura.LeilaoOnline.WebApp.Dados
+{
+    public class LeilaoDao
+    {
+        private readonly AppDbContext _context;
+
+        public LeilaoDao()
+        {
+            _context = new AppDbContext();
+        }
+        
+        public IEnumerable<Categoria> BuscarCategorias()
+        {
+            return _context.Categorias.ToList();
+        }
+
+        public IEnumerable<Leilao> BuscarLeiloes()
+        {
+            return _context.Leiloes
+                .Include(l => l.Categoria)
+                .ToList();
+        }
+
+        public Leilao BuscarPorId(int id)
+        {
+            return _context.Leiloes.FirstOrDefault(l => l.Id == id);
+        }
+
+        public IEnumerable<Leilao> PesquisarLeiloes(string termo)
+        {
+            var leiloes = _context.Leiloes
+                .Include(l => l.Categoria)
+                .Where(l => string.IsNullOrWhiteSpace(termo) || 
+                            l.Titulo.ToUpper().Contains(termo.ToUpper()) || 
+                            l.Descricao.ToUpper().Contains(termo.ToUpper()) ||
+                            l.Categoria.Descricao.ToUpper().Contains(termo.ToUpper())
+                );
+            return leiloes;
+        }
+
+        public void Incluir(Leilao leilao)
+        {
+            _context.Leiloes.Add(leilao);
+            _context.SaveChanges();
+        }
+        
+        public void Editar(Leilao leilao)
+        {
+            _context.Leiloes.Update(leilao);
+            _context.SaveChanges();
+        }
+
+        public void Excluir(Leilao leilao)
+        {
+            _context.Leiloes.Remove(leilao);
+            _context.SaveChanges();
+        }
+    }
+}
